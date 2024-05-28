1b3539cc7ee76449aff5d97abb6f962d741045be
SubburamK <www.subburamk@gmail.com>
2024-05-28 20:13:37 +0530

    sidebar

--- client/src/components/SmallSideBar.jsx
+++ client/src/components/SmallSideBar.jsx
@@ -1,9 +1,33 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Logo from "./Logo";
+import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 const SmallSideBar = () => {
+  const {showSidebar,toggleSidebar} = useDashboardContext();
+
   return (
-    <Wrapper>SmallSideBar</Wrapper>
+    <Wrapper>
+      <div 
+      className={
+        showSidebar?"sidebar-container show-sidebar":
+        'sidebar-container'
+      }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
-export default SmallSideBar
+export default SmallSideBar;
