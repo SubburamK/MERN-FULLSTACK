baf5d719252eb61543e6242200c7794126d6dadb
SubburamK <www.subburamk@gmail.com>
2024-05-31 15:19:48 +0530

    thunder client

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 const app = express();
+import morgan from 'morgan';
+
+
+app.use(morgan('dev'));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.post('/', (req,res)=>{
+    console.log(req);
+    res.json({message:'data recieved',data: req.body});
+})
 
 app.listen(5100,()=>{
     console.log('server running...');
