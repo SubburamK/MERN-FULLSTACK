83b02ab5a565e04c68313b04414238ec7591c2d1
SubburamK <www.subburamk@gmail.com>
2024-05-31 12:14:26 +0530

    install server

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo } from '../components';
+import FormRow from '../components/FormRow';
+const Login = () => {
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type='email' name='email' required />
+        <FormRow type='password' name='password' required />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>Register</Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login
