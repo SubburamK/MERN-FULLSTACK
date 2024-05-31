0ce0c2020c759da3f58117a57d4eec563a25931b
SubburamK <www.subburamk@gmail.com>
2024-05-31 16:14:33 +0530

    create first route

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+let jobs = [
+    {id:nanoid(),company:'apple',position:'front-end'},
+    {id:nanoid(),company:'google',position:'back-end'},
+];
 
 
-app.use(morgan('dev'));
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -14,8 +24,15 @@ app.get('/', (req, res) => {
 app.post('/', (req,res)=>{
     console.log(req);
     res.json({message:'data recieved',data: req.body});
-})
+});
 
-app.listen(5100,()=>{
-    console.log('server running...');
+app.get('/app/v1/jobs',(req, res) => {
+    res.status(200).json({jobs})
+});
+
+
+const port = process.env.PORT || 5100
+
+app.listen(port,()=>{
+    console.log(`server running on PORT ${port}...`);
 });
